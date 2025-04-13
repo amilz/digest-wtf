1f97d4f07f6578d9c7965dc7f172a5b3108daff3
amilz <85324096+amilz@users.noreply.github.com>
2025-04-13 10:55:34 -0700

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,68 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.digest.wtf/'),
   title: "digest.wtf",
   description: "Personalized web content aggregation service",
-}
+  applicationName: "digest.wtf",
+  authors: [{ name: "amilz", url: "https://github.com/amilz" }],
+  keywords: [
+    "AI",
+    "Tracking",
+    "Monitoring",
+    "Web",
+    "Content",
+    "Aggregator",
+    "Search",
+    "Next.js",
+    "Vercel"
+  ],
+  openGraph: {
+    type: "website",
+    siteName: "digest.wtf",
+    title: "digest.wtf",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "digest.wtf - Personalized web content aggregation service",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://www.digest.wtf/",
+    creator: "https://github.com/amilz",
+    title: "digest.wtf",
+    description: "Personalized web content aggregation service",
+    images: ["/og-image.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+  },
+  manifest: "/site.webmanifest",
+};
+
 
 export default function RootLayout({
   children,
